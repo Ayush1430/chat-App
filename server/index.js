407dd06265f5ef166b1e0985b8e16cfeaf981a0c
Ayush <ayushsoni18182@gmail.com>
2023-01-13 17:50:32 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import cors from "cors";
+import express from "express";
+import http from "http";
+import mongoose from "mongoose";
+import path from "path";
+import { Server } from "socket.io";
+import { fileURLToPath } from "url";
+import router from "./api/routes.js";
+import sockets from "./socket/sockets.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
+
+const app = express();
+const PORT = process.env.PORT;
+
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.use("/", router);
+
+io.on("connection", sockets);
+
+httpServer.listen(PORT, () => {
+  console.log("Server is running ");
+});
